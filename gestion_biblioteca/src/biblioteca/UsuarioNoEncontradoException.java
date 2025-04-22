02f82e52a686cbd7efba5f8a9c4b8f6e48f35e4b
valeartola <mv.artola@alumno.um.edu.ar>
2025-04-22 15:19:57 -0300

    creacion de clases excepciones

---
+++ gestion_biblioteca/src/biblioteca/UsuarioNoEncontradoException.java
@@ -0,0 +1,7 @@
+package biblioteca;
+
+public class UsuarioNoEncontradoException extends RuntimeException {
+    public UsuarioNoEncontradoException(String message) {
+        super(message);
+    }
+}
